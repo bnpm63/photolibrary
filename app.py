3cfb1fa40b7ed83c96f5b8748f80000e5c2be2e2
B3n <bph2275@gmail.com>
2024-10-03 01:40:50 -0700

    added html

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, redirect, url_for, render_template
+import boto3
+import os
+
+app = Flask(__name__)
+s3 = boto3.client('s3')
+
+@app.route('/')
+def index():
+    
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    file = request.files['file']
+    if file:
+        s3.upload_fileobj(
+            file,
+            'cloud-photo-lib',  
+            file.filename,
+            ExtraArgs={'ACL': 'public-read'}  
+        )
+        
+        return redirect(url_for('index'))
+    return "Failed to upload"
+
+if __name__ == '__main__':
+    app.run(debug=True)
